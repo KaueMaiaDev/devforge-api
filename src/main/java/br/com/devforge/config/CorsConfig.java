5ebe28e42cc9c9fdd8295100af997ebdd9c81f49
maia469 <kaueflagoroso@gmail.com>
2025-11-19 12:37:01 -0300

    Fix: Adicionando anotação @Configuration no CORS

--- src/main/java/br/com/devforge/config/CorsConfig.java
+++ src/main/java/br/com/devforge/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
